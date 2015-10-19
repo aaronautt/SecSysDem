79f9224a5ac5793744ef81e5255a5e7c2825874a
Luke Ganzevoort <lukeganzevoort@gmail.com>
2015-10-18 22:24:27 -0400

    Added a defines header to be included in drivers. The i2c driver was editted to use the F_CPU frequency from the secSysDefines.h file

---
+++ secSysDefines.h
@@ -0,0 +1,17 @@
+/*
+ * secSysDefines.h
+ *
+ * Created: 10/18/2015 10:16:40 PM
+ *  Author: Luke
+ */ 
+
+
+#ifndef SECSYSDEFINES_H_
+#define SECSYSDEFINES_H_
+
+
+#define F_CPU	8000000L
+
+
+
+#endif /* SECSYSDEFINES_H_ */
