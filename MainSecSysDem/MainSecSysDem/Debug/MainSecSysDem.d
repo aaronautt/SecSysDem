522d12d9a6d87723c0e33b01244fe6d5dc792062
void191 <void191@hotmail.com>
2015-10-25 21:38:57 -0400

    kkkk

--- MainSecSysDem/MainSecSysDem/Debug/MainSecSysDem.d
+++ MainSecSysDem/MainSecSysDem/Debug/MainSecSysDem.d
@@ -15,7 +15,8 @@ MainSecSysDem.d MainSecSysDem.o: .././MainSecSysDem.c \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stddef.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\stdlib.h \
  .././secSysDefines.h .././WATCH_DOG.h .././PIR_DRIVER.h \
- .././i2c_driver.h .././uart.h .././Hall_Sensors.h
+ .././i2c_driver.h .././uart.h .././Hall_Sensors.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\string.h
 
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -58,3 +59,5 @@ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-g
 .././uart.h:
 
 .././Hall_Sensors.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\string.h:
