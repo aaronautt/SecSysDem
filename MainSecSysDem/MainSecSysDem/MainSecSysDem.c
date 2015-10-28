8484c11d867caae6317af5c104d4c16f49460bc0
void191 <void191@hotmail.com>
2015-10-27 21:49:58 -0400

    cleaned master

--- MainSecSysDem/MainSecSysDem/MainSecSysDem.c
+++ MainSecSysDem/MainSecSysDem/MainSecSysDem.c
@@ -25,29 +25,10 @@
 
 int main(void)
 {
-	// Initialize the UART
-	USART_Init(MYUBRR);
-	stdout = &uart_output;
-	stdin  = &uart_input;
-	I2C_Init();
 	
-	int i, out=0;
-	char word[6] = "hello";
-	
-	
-	I2C_WriteRegister(RTC_EEP_ADDR, 0, 1);
-	
-	out=I2C_ReadRegister(RTC_EEP_ADDR, 0);
-	printf("hello\n");
-	_delay_ms(1000);
-	
-	//HALL_init();
     while(1)
     {
 		
-		/*
-         if(!(Hall_Window_check())) printf("open!\n");
-		 else; //null//
-		 */
+		
     }
 }
