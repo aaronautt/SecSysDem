847194bbffc4d45329ea6c59f9e62ecd82ecfa5a
Luke Ganzevoort <lukeganzevoort@gmail.com>
2015-10-28 19:44:46 -0400

    Added rgbLED library for turning on and off the RGB.

---
+++ MainSecSysDem/MainSecSysDem/rgbLed.h
@@ -0,0 +1,28 @@
+/*
+ * rgbLed.h
+ *
+ * Created: 10/28/2015 7:28:44 PM
+ *  Author: Luke
+ */ 
+
+
+#ifndef RGBLED_H_
+#define RGBLED_H_
+
+// Initializes the rgb ddr and turns it off
+void rgb_init();
+
+// Turns on the RGB led to red
+void rgb_red();
+
+// Turns on the RGB led to red
+void rgb_green();
+
+// Turns on the RGB led to red
+void rgb_blue();
+
+// Turns off the RGB led
+void rgb_off();
+
+
+#endif /* RGBLED_H_ */
