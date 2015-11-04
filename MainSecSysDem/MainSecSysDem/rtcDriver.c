edbbfcb050648335c9d71e8bf70afe7a424ecca0
Luke Ganzevoort <lukeganzevoort@gmail.com>
2015-11-04 16:22:41 -0500

    initiated files

---
+++ MainSecSysDem/MainSecSysDem/rtcDriver.c
@@ -0,0 +1,6 @@
+/*
+ * rtcDriver.c
+ *
+ * Created: 11/4/2015 4:21:48 PM
+ *  Author: Luke
+ */ 
---
+++ MainSecSysDem/MainSecSysDem/rtcDriver.h
@@ -0,0 +1,16 @@
+/*
+ * rtcDriver.h
+ *
+ * Created: 11/4/2015 4:21:59 PM
+ *  Author: Luke
+ */ 
+
+
+#ifndef RTCDRIVER_H_
+#define RTCDRIVER_H_
+
+
+
+
+
+#endif /* RTCDRIVER_H_ */
