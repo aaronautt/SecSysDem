560ea4ad110f9058b52a82dbf60e925e374cf686
void191 <void191@hotmail.com>
2015-11-16 17:10:23 -0500

    adding updated lcd display drivers, still need work

--- MainSecSysDem/MainSecSysDem/lcd_moving.h
+++ MainSecSysDem/MainSecSysDem/lcd_moving.h
@@ -16,6 +16,9 @@ void Lcd_Light_bright(uint8_t brigtness);
 void Scrolling_Text(char message[]);
 void Scrolling_Text_single(char input[], uint8_t position);
 void display_temp(uint8_t temp);
+void display_status(uint8_t status, uint8_t location);
+void display_main_menu(void);
+void display_last_five(void);
 void array_shift(char message[]);
 
 #endif /* lcd_moving_H_ */
