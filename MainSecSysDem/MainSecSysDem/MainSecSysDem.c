9eada1bfdc0beab8871742c4bb8795274af06685
Luke Ganzevoort <lukeganzevoort@gmail.com>
2015-11-21 23:17:30 -0500

    prepared main for merging

--- MainSecSysDem/MainSecSysDem/MainSecSysDem.c
+++ MainSecSysDem/MainSecSysDem/MainSecSysDem.c
@@ -29,58 +29,18 @@
 #define MYUBRR F_CPU/8/BAUD-1
 
 
-// int main(void)
-// {
-// 	// Initialize the UART
-// 	USART_Init(MYUBRR);
-// 	stdout = &uart_output;
-// 	stdin  = &uart_input;
-// 	
-// 	I2C_Init();
-// 
-// 
-//     while(1)
-// 	{
-// 
-// 	}
-// }
-
-int main()
+int main(void)
 {
-	// ----- 5ms Timer -----
-	// Reset timer0
-	TCNT0 = 0;
-	// set timer 0 to CTC mode
-	TCCR0A |= (1<<WGM01);
-	// set 5 ms interval, 78 cycles
-	OCR0A = 255;
-	//set prescaler to 1024
-	TCCR0B |= (1<<CS02) | (1<<CS00);
-	//set the timer output compare 0A to on
-	TIMSK0 |= (1<<OCIE0A);
-	// Enable global interrupts
-	sei();	
+	// Initialize the UART
+	USART_Init(MYUBRR);
+	stdout = &uart_output;
+	stdin  = &uart_input;
 	
-	rgb_init();
-	pushButton_init();
-	//rgb_blue();
-	rgb_flash_start();
+	I2C_Init();
 
-	while(1)
+
+    while(1)
 	{
-		rgb_flash_check();
-		
-		if(pushButtonRead())
-		{
-			rgb_flash_stop();
-		}
-		_delay_ms(32);
-	}
-	
-	return(0);
-}
 
-ISR(TIMER0_COMPA_vect)
-{
-	rgb_flash_32msInterrupt();
+	}
 }
