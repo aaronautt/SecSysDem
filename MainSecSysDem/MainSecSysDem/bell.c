bc0d725aca89072455485d1729a3b2bbb4bca2d1
Luke Ganzevoort <lukeganzevoort@gmail.com>
2015-11-25 15:40:41 -0500

    updated Bell

--- MainSecSysDem/MainSecSysDem/bell.c
+++ MainSecSysDem/MainSecSysDem/bell.c
@@ -9,20 +9,80 @@
 #include "secSysDefines.h"
 #include "ioExpander.h"
 
+enum bellStatus{
+	NO_CHANGE,
+	TURN_OFF,
+	TURN_ON
+};
+
+static uint8_t changeBell=NO_CHANGE;
+static uint8_t bellEnabled=0;
+
 // Initiates the audible sounding bell by turning it off
 void bell_init()
 {
-	ioExpClrBit(BELL_ALARM_PIN_IOEX);
+	ioExpSetBit(BELL_ALARM_PIN_IOEX);
 }
 
 // Turns on the audible sounding bell
 void bell_on()
 {
-	ioExpSetBit(BELL_ALARM_PIN_IOEX);
+	ioExpClrBit(BELL_ALARM_PIN_IOEX);
 }
 
 // Turns off the audible sounding bell
 void bell_off()
 {
-	ioExpClrBit(BELL_ALARM_PIN_IOEX);
+	ioExpSetBit(BELL_ALARM_PIN_IOEX);
+}
+
+void bell_enable()
+{
+	bellEnabled = 1;
+	changeBell = NO_CHANGE;
+	bell_off();
+}
+
+void bell_disable()
+{
+	bellEnabled = 0;
+	bell_off();
+}
+
+void bell_UpdateStatus()
+{
+	if(bellEnabled)
+	{
+		if(changeBell == TURN_ON)
+		{
+			changeBell = NO_CHANGE;
+			bell_on();
+		}
+		else if(changeBell == TURN_OFF)
+		{
+			changeBell = NO_CHANGE;
+			bell_off();
+		}	
+	}
+}
+
+void bell_InterruptFunction()
+{
+	static uint8_t counter=0;
+	if(bellEnabled)
+	{
+		counter++;
+		if(counter == 1)
+		{
+			changeBell = TURN_ON;
+		}
+		else if(counter == 2)
+		{
+			changeBell = TURN_OFF;
+		}
+		else if(counter > 16)
+		{
+			counter = 0;
+		}	
+	}
 }
--- MainSecSysDem/MainSecSysDem/bell.h
+++ MainSecSysDem/MainSecSysDem/bell.h
@@ -13,10 +13,19 @@
 void bell_init();
 
 // Turns on the audible sounding bell
-void bell_on();
+//void bell_on();
 
 // Turns off the audible sounding bell
-void bell_off();
+//void bell_off();
 
+// Enable the bell
+void bell_enable();
+// Disable the bell
+void bell_disable();
+// This function actually turns on and off the bell at the proper intervals.
+// It must be placed in the while loop.
+void bell_UpdateStatus();
+// This function is supposed to be in the 32ms interrupt ISR
+void bell_InterruptFunction();
 
 #endif /* BELL_H_ */
